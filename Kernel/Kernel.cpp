366b249ee3b578e7d1e39493b3c25c03bb037adc
TheGeneralSecretary <TheGeneralSecretary@protonmail.com>
2023-01-17 23:20:07 -0500

    Added Simple VGA Driver

--- Kernel/Kernel.cpp
+++ Kernel/Kernel.cpp
@@ -1,5 +1,19 @@
+#include <Kernel/Drivers/VGA.h>
+#include <Kernel/KPrintf.h>
+#include <Kernel/KPanic.h>
+
+namespace Kernel
+{
+	void InitKernel()
+	{
+		if (Drivers::VGA::Init() != 0)
+			KPanic("Failed To Initialize VGA Driver\n");
+
+		KPrintf("Kernal Initialized\n");
+	}
+}
+
 extern "C" void KMain()
 {
-	int x = 10;
-	(void)x;
+	Kernel::InitKernel();
 }
